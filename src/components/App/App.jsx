e16f381fb9081c837b80893e5dc302fc77fa64fa
ProskurinV <proskyrinv.g@gmail.com>
2022-10-14 17:24:20 +0300

    hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,61 +1,68 @@
 // import { Box } from '../Box';
-import { Component } from 'react';
+import { useState } from 'react';
 import Section from 'components/Section';
 import { MainBox } from './App.styled';
 import Notification from 'components/Notification';
 import Statistics from 'components/Statistics';
 import FeedbackOptions from 'components/FeedbackOptions';
 
-export default class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const types = { good, neutral, bad };
+
+  const stateKeys = Object.keys(types);
+
+  const handleUpdateFeedback = type => {
+    switch (type) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+
+      default:
+        break;
+    }
   };
 
-  stateKeys = Object.keys(this.state);
-
-  options = this.stateKeys;
-
-  handleFeedbackСlick = option => {
-    this.setState(prevState => ({
-      [option]: prevState[option] + 1,
-    }));
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
 
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((acc, value) => acc + value, 0);
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100) || 0;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    return Math.round((this.state.good / this.countTotalFeedback()) * 100) || 0;
-  };
+  return (
+    <MainBox>
+      <Section title="Please, leave feedback">
+        <FeedbackOptions
+          options={stateKeys}
+          onLeaveFeedback={handleUpdateFeedback}
+        />
 
-  render() {
-    const { good, neutral, bad } = this.state;
-
-    return (
-      <MainBox>
-        <Section title="Please, leave feedback">
-          <FeedbackOptions
-            options={this.stateKeys}
-            onLeaveFeedback={this.handleFeedbackСlick}
+        {countTotalFeedback() === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            title="Statistics"
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-
-          {this.countTotalFeedback() === 0 ? (
-            <Notification message="There is no feedback" />
-          ) : (
-            <Statistics
-              title="Statistics"
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          )}
-        </Section>
-      </MainBox>
-    );
-  }
+        )}
+      </Section>
+    </MainBox>
+  );
 }
